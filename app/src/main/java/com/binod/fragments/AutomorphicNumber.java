85fb536b347a2d41978b126b01727b43e0c631ca
Binodmagar <ranabinod615@gmail.com>
2019-12-10 16:34:44 +0545

    Automorphic, palindrome design and code finished. Simple interest, swapping number desing and code started.

--- app/src/main/java/com/binod/fragments/AutomorphicNumber.java
+++ app/src/main/java/com/binod/fragments/AutomorphicNumber.java
@@ -8,25 +8,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.binod.classassignment2.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AutomorphicNumber extends Fragment {
+public class AutomorphicNumber extends Fragment implements View.OnClickListener{
 
-
-    public AutomorphicNumber() {
-        // Required empty public constructor
-    }
+    EditText etAutomorphic;
+    Button btnAutomorphicC;
+    TextView tvMessage;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_automorphic_number, container, false);
+        View view = inflater.inflate(R.layout.fragment_automorphic_number, container, false);
+
+        etAutomorphic = view.findViewById(R.id.etAutomorphic);
+        btnAutomorphicC = view.findViewById(R.id.btnAutomorphicC);
+        tvMessage = view.findViewById(R.id.tvMessage);
+
+        btnAutomorphicC.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        int num = Integer.parseInt(etAutomorphic.getText().toString());
+        int square_num = num * num;
+
+        String squarenum = Integer.toString(num);
+        String square = Integer.toString(square_num);
+
+        if(square.endsWith(squarenum)){
+            tvMessage.setText(num + " is Automorphic number");
+        }else {
+            tvMessage.setText(num + " is not Automorphic number");
+        }
+
+    }
 }
--- app/src/main/java/com/binod/fragments/PalindromeNumber.java
+++ app/src/main/java/com/binod/fragments/PalindromeNumber.java
@@ -8,25 +8,53 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.binod.classassignment2.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PalindromeNumber extends Fragment {
+public class PalindromeNumber extends Fragment implements View.OnClickListener{
 
-
-    public PalindromeNumber() {
-        // Required empty public constructor
-    }
+    EditText etPalindrome;
+    Button btnPalindromeC;
+    TextView tvMessage;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_palindrome_number, container, false);
+        View view = inflater.inflate(R.layout.fragment_palindrome_number, container, false);
+
+        etPalindrome = view.findViewById(R.id.etPalindrome);
+        tvMessage = view.findViewById(R.id.tvMessage);
+        btnPalindromeC = view.findViewById(R.id.btnPalindromeC);
+
+        btnPalindromeC.setOnClickListener(this);
+
+        return  view;
     }
 
+    @Override
+    public void onClick(View v) {
+        int num = Integer.parseInt(etPalindrome.getText().toString());
+        int reInt = 0, rem, originalInt;
+        originalInt = num;
+        // reversed integer is stored in variable
+        while( num != 0 )
+        {
+            rem = num % 10;
+            reInt = reInt * 10 + rem;
+            num  /= 10;
+        }
+        if (originalInt == reInt){
+            tvMessage.setText(originalInt + " is Palindrome number");
+        }else {
+            tvMessage.setText(originalInt + " is not a Palindrome number");
+        }
+    }
 }

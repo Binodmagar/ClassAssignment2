0d75acd5fffc6589927937cfcaccfd206cba8e70
Binodmagar <ranabinod615@gmail.com>
2019-12-10 12:57:52 +0545

    class assignment started and fragments packages created along wiht area of circle, armstrong, automorphic, palindrome, simpleinterest, swapping number created repectively with .xml file

--- app/src/main/java/com/binod/classassignment2/MainActivity.java
+++ app/src/main/java/com/binod/classassignment2/MainActivity.java
@@ -1,14 +1,41 @@
 package com.binod.classassignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnArea, btnArmstrong, btnAutomorphic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //binding
+        btnArea = findViewById(R.id.btnArea);
+        btnArmstrong = findViewById(R.id.btnArmstrong);
+        btnAutomorphic = findViewById(R.id.btnAutomorphic);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnArea :
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.commit();
+                btnArea.setText("Load Armstrong number");
+                break;
+
+
+
+
+        }
     }
 }
